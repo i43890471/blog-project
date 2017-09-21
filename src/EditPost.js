6255264ffa60a640cf74842099889f9b3e51a776
i4389071 <idil.erhan@gmail.com>
2017-09-21 17:30:02 -0400

    styles for edit post

--- src/EditPost.js
+++ src/EditPost.js
@@ -17,7 +17,7 @@ class EditPost extends React.Component {
   _handleEditPost = event => {
     event.preventDefault();
     const { post } = this.props;
-    console.log('putting')
+    console.log("putting");
     axios
       .put(
         `/api/posts/${post.id}`,
@@ -32,7 +32,7 @@ class EditPost extends React.Component {
         }
       )
       .then(response => {
-        console.log('EDIT RESPONSE: ', response);
+        console.log("EDIT RESPONSE: ", response);
         if (response.data.success) {
           alert("Updated");
           this.props.onChange();
@@ -59,24 +59,66 @@ class EditPost extends React.Component {
     const { post } = this.props;
 
     return (
-      <div>
-        <form onSubmit={this._handleEditPost}>
-          <input
-            type="text"
-            value={this.state.title}
-            name="title"
-            onChange={this._handleInputChange}
-          />
-          <textarea
-            value={this.state.content}
-            name="content"
-            onChange={this._handleInputChange}
-          />
-          <input type="submit" value="Edit Post" />
-          <button onClick={this._handleDeletePost}>Delete post</button>
-        </form>
+      <div className="container">
+        <form
+          className="form-horizontal"
+          role="form"
+          onSubmit={this._handleEditPost}
+        >
+          <h2 className="text-center ">Edit Your Article</h2>
 
-       
+          <div className="form-group">
+            <label for="firstName" className="col-sm-3 control-label">
+              TITLE
+            </label>
+
+            <div className="col-sm-9">
+              <input
+                className="form-control"
+                type="text"
+                name="title"
+                onChange={this._handleInputChange}
+                value={this.state.title}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label for="content" className="col-sm-3 control-label">
+              CONTENT
+            </label>
+
+            <div className="col-sm-9">
+              <textarea
+                className="form-control"
+                rows="6"
+                name="content"
+                onChange={this._handleInputChange}
+                value={this.state.content}
+              />
+
+              <span className="help-block">Description of your article</span>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="col-sm-4 col-sm-offset-4">
+              <button type="submit" className="btn btn-info btn-block">
+                Edit Post
+              </button>
+            </div>
+
+            <div className="col-sm-4 ">
+              <button
+                type="submit"
+                className="btn btn-danger btn-block"
+                onClick={this._handleDeletePost}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
